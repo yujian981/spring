c5ceadb852eb9b91b327252ffe253ad33cfa5a19
Your Name <15116952620@163.com>
2019-08-22 10:07:32 +0800

    简单实现 redis + session 共享

---
+++ springboot-session/src/main/java/com/example/springbootsession/testController.java
@@ -0,0 +1,33 @@
+package com.example.springbootsession;
+
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author Cty
+ */
+@RestController
+public class testController {
+
+
+    @Value("${server.port}")
+    private Integer port;
+
+    @GetMapping("/set")
+    public String set(HttpSession session) {
+        session.setAttribute("user", "javaboy");
+        return String.valueOf(port);
+    }
+
+    @GetMapping("/get")
+    public String get(HttpSession session) {
+        return session.getAttribute("user") + ":" + port;
+    }
+
+
+}
